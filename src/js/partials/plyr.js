c5fec2cede0544ea5804f93a1d844eb3b7e2c1eb
HorbynkoIvan <horbynko.i@gmail.com>
2020-11-13 12:21:39 +0200

    config slider

--- src/js/partials/plyr.js
+++ src/js/partials/plyr.js
@@ -7,6 +7,52 @@ export function plyrPlayer() {
         {
             fullscreen: { enabled: false, fallback: true, iosNative: false },
             hideControls: true,
-            controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'],
+            controls: [
+               // 'play-large',
+                // 'play',
+                // 'progress',
+                // 'current-time',
+                // 'mute',
+                // 'volume',
+                // 'captions',
+                // 'settings',
+                // 'pip',
+                // 'airplay',
+                // 'fullscreen'
+            ],
         });
+
+
+    // Bind event listener
+    function on(selector, type, callback) {
+        document.querySelector(selector).addEventListener(type, callback, false);
+    }
+
+    // Loop through each instance
+    players.forEach(function(instance) {
+        // Play
+        // on('.js-play', 'click', function() {
+        //     instance.play();
+        // });
+        //
+        // // Pause
+        // on('.js-pause', 'click', function() {
+        //     instance.pause();
+        // });
+        //
+        // // Stop
+        // on('.js-stop', 'click', function() {
+        //     instance.stop();
+        // });
+        //
+        // // Rewind
+        // on('.js-rewind', 'click', function() {
+        //     instance.rewind();
+        // });
+        //
+        // // Forward
+        // on('.js-forward', 'click', function() {
+        //     instance.forward();
+        // });
+    });
 }
